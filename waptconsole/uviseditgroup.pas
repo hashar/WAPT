2efaef2921e38cafe664fa99f709884ee53dc13a
htouvet <hubert.touvet@tranquil-it-systems.fr>
2014-02-03 18:20:06 +0100

    bugfix status of management workstation is updated while managing other workstations with machine packages F5 updates only current page.

---
+++ waptconsole/uviseditgroup.pas
@@ -0,0 +1,57 @@
+unit uviseditgroup;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Buttons, StdCtrls, sogrid,superobject;
+
+type
+
+  { TVisEditGroup }
+
+  TVisEditGroup = class(TForm)
+    BitBtn2: TBitBtn;
+    Button5: TButton;
+    Eddescription: TLabeledEdit;
+    EdGroup: TLabeledEdit;
+    GridDepends: TSOGrid;
+    GridHosts: TSOGrid;
+    GridPackages: TSOGrid;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Panel4: TPanel;
+  private
+    FGroup: String;
+    members:ISuperobject;
+    procedure SetGroup(AValue: String);
+    { private declarations }
+  public
+    { public declarations }
+    property Group : String read FGroup write SetGroup;
+  end;
+
+var
+  VisEditGroup: TVisEditGroup;
+
+implementation
+uses tiscommon,waptcommon;
+{$R *.lfm}
+
+{ TVisEditGroup }
+
+procedure TVisEditGroup.SetGroup(AValue: String);
+begin
+  if FGroup=AValue then Exit;
+  FGroup:=AValue;
+  members :=  WAPTServerJsonGet('/hosts_by_group/'+Fgroup, [], WaptUseLocalConnectionProxy);
+  EdGroup.Text:=Fgroup;
+  GridHosts.Data := members;
+  GridHosts.Header.AutoFitColumns(False);
+end;
+
+end.
+
